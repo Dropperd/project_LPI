d80544929cd84de12a64ade4fedd64c19622f770
Dropperd <36640@ufp.edu.pt>
2022-04-06 18:51:11 +0100

    Componente do Chart de área

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgApexchartsModule } from "ng-apexcharts";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ import { FormsModule } from "@angular/forms";
+ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+ import { 
+	IgxCategoryChartModule,
+	IgxLegendModule
+ } from "igniteui-angular-charts";
+ 
+ import { AreaChart } from "./AreaChartComponent/AreaChart.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaChart
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgApexchartsModule,
+    BrowserAnimationsModule,
+	  FormsModule,
+  	IgxCategoryChartModule,
+  	IgxLegendModule
   ],
   providers: [],
   bootstrap: [AppComponent]
